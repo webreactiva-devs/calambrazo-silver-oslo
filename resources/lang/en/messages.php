c40c458489b75af37d97b5a3b3e97743f3e87c6c
Sergio González <sergiogonzalezmerino@gmail.com>
2024-11-24 20:38:53 +0100

    Added translates for Device vote control

--- resources/lang/en/messages.php
+++ resources/lang/en/messages.php
@@ -3,6 +3,7 @@
 return [
     "vote_success" => "Vote successfully registered",
     "vote_error" => "You have already voted for this feedback",
+    "device_vote_error"=> "You have already voted with this device",
     "feedback_update_success" => "Your feedback has been successfully updated",
     "feedback_create_success"=> "Your feedback has been successfully created",
     "feedback_delete_success"=> "Feedback has been deleted",
--- resources/lang/es/messages.php
+++ resources/lang/es/messages.php
@@ -3,6 +3,7 @@
 return [
     "vote_success" => "Voto registrado con éxito",
     "vote_error" => "Ya has votado por este feedback",
+    "device_vote_error"=> "Ya has votado con este dispositivo",
     "feedback_update_success" => "Feedback actualizado correctamente",
     "feedback_create_success"=> "Feedback creado correctamente",
     "feedback_delete_success"=> "Feedback eliminado",
