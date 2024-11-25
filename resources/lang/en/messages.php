14dfc06edd82d62d164eedb74afbe0152d80d96a
Sergio González <sergiogonzalezmerino@gmail.com>
2024-11-25 10:12:35 +0100

    Added translates for Ip vote control

--- resources/lang/en/messages.php
+++ resources/lang/en/messages.php
@@ -4,6 +4,7 @@ return [
     "vote_success" => "Vote successfully registered",
     "vote_error" => "You have already voted for this feedback",
     "device_vote_error"=> "You have already voted with this device",
+    "ip_vote_error"=> "You have already voted with this IP",
     "feedback_update_success" => "Your feedback has been successfully updated",
     "feedback_create_success"=> "Your feedback has been successfully created",
     "feedback_delete_success"=> "Feedback has been deleted",
