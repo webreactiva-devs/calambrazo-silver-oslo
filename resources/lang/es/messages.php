19537447f3bf6dbca9a363748cfda438d30a99d5
Sergio González <sergiogonzalezmerino@gmail.com>
2024-11-25 15:58:13 +0100

    Added translate for vote messages fingerprint EN & ES

--- resources/lang/es/messages.php
+++ resources/lang/es/messages.php
@@ -5,6 +5,7 @@ return [
     "vote_error" => "Ya has votado por este feedback",
     "device_vote_error"=> "Ya has votado con este dispositivo",
     "ip_vote_error"=> "Ya has votado con esta IP",
+    "fingerprint_vote_error"=> "Espabilao, se nota que eres Malandrin! Pero este voto ya lo has hecho",
     "feedback_update_success" => "Feedback actualizado correctamente",
     "feedback_create_success"=> "Feedback creado correctamente",
     "feedback_delete_success"=> "Feedback eliminado",
