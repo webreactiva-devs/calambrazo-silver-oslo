c1c0f5e86e34de3f9f4bc25f7d6342f07b8aaa45
Sergio González <sergiogonzalezmerino@gmail.com>
2024-12-23 11:59:57 +0100

    test: camelCase variable naming for browser instances in DifferentDevicesTest

--- tests/Browser/DifferentDevicesTest.php
+++ tests/Browser/DifferentDevicesTest.php
@@ -7,7 +7,7 @@ use Illuminate\Foundation\Testing\DatabaseMigrations;
 uses(DatabaseMigrations::class);
 
 test('Diferentes dispositivos emulados', function () {
-    $this->browse(function (Browser $iPhone, Browser $Pixel, Browser $Mac, Browser $Windows) {
+    $this->browse(function (Browser $iPhone, Browser $pixel, Browser $mac, Browser $windows) {
         $user1 = User::factory()->create([
             'email' => 'dani@malandrin.com',
             'password' => bcrypt('12345678'),
@@ -39,10 +39,10 @@ test('Diferentes dispositivos emulados', function () {
             ->assertSee('Ya has votado')
             ->assertAttribute('#vote-debug', 'data-reason', 'device');
 
-        $Pixel->script('Object.defineProperty(navigator, "userAgent", {get: () => "Mozilla/5.0 (Linux; Android 10; Pixel 4 XL) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Mobile Safari/537.36"});');
+        $pixel->script('Object.defineProperty(navigator, "userAgent", {get: () => "Mozilla/5.0 (Linux; Android 10; Pixel 4 XL) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Mobile Safari/537.36"});');
         sleep(1);
-        $Pixel->resize(1440, 3040);
-        $Pixel->visit('/')
+        $pixel->resize(1440, 3040);
+        $pixel->visit('/')
             ->waitForText('Feedbacks')
             ->assertSee('Feedbacks')
             ->waitFor('@vote-button')
@@ -50,10 +50,10 @@ test('Diferentes dispositivos emulados', function () {
             ->assertSee('Ya has votado')
             ->assertAttribute('#vote-debug', 'data-reason', 'device');
 
-        $Mac->script('Object.defineProperty(navigator, "userAgent", {get: () => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Version/14.0 Safari/537.36"});');
+        $mac->script('Object.defineProperty(navigator, "userAgent", {get: () => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Version/14.0 Safari/537.36"});');
         sleep(1);
-        $Mac->resize(2560, 1600);
-        $Mac->visit('/')
+        $mac->resize(2560, 1600);
+        $mac->visit('/')
             ->waitForText('Feedbacks')
             ->assertSee('Feedbacks')
             ->waitFor('@vote-button')
@@ -61,10 +61,10 @@ test('Diferentes dispositivos emulados', function () {
             ->assertSee('Ya has votado')
             ->assertAttribute('#vote-debug', 'data-reason', 'device');
 
-        $Windows->script('Object.defineProperty(navigator, "userAgent", {get: () => "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"});');
+        $windows->script('Object.defineProperty(navigator, "userAgent", {get: () => "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"});');
         sleep(1);
-        $Windows->resize(1920, 1080);
-        $Windows->visit('/')
+        $windows->resize(1920, 1080);
+        $windows->visit('/')
             ->waitForText('Feedbacks')
             ->assertSee('Feedbacks')
             ->waitFor('@vote-button')
